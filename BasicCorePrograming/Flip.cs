981f1659c1218ffb07090673cae9859922fe6bd2
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-02-07 09:24:47 +0530

    Flip Coin and print percentage of Heads and Tails

---
+++ BasicCorePrograming/Flip.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograming
+{
+    internal class Flip
+    {
+        int countHead = 0, countTail = 0;
+        public void Coin()
+        {
+            Console.WriteLine("Enter The number of times to Flip Coin ");
+            int N_TimeFlipCoin = Convert.ToInt32(Console.ReadLine());
+
+            Random random = new Random();
+
+            for (int i = 0; i < N_TimeFlipCoin; i++)
+            {
+                double numCheck = random.NextDouble();
+                if (numCheck < 0.5)
+                    countHead++;
+                else
+                    countTail++;
+            }
+
+            Console.WriteLine(countHead + " " + countTail);
+            float headPersentage = (float)countHead / N_TimeFlipCoin * 100;
+            float tailPersentage = (float)countTail / N_TimeFlipCoin * 100;
+            Console.WriteLine(headPersentage + " " + tailPersentage);
+        }
+
+        public void Coin1()
+        {
+            Console.WriteLine("Enter The number of times to Flip Coin ");
+            int N_TimeFlipCoin = Convert.ToInt32(Console.ReadLine());
+            int Head = 1;
+            Random random = new Random();
+
+            for (int i = 0; i < N_TimeFlipCoin; i++)
+            {
+                int numCheck = random.Next(2);
+
+                if (Head == numCheck)
+                    countHead++;
+                else
+                    countTail++;
+            }
+
+            Console.WriteLine(countHead + " " + countTail);
+            float headPersentage = (float)countHead / N_TimeFlipCoin * 100;
+            float tailPersentage = (float)countTail / N_TimeFlipCoin * 100;
+            Console.WriteLine(headPersentage + " " + tailPersentage);
+        }
+    }
+}
---
+++ BasicCorePrograming/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Flip Coin ");
+            Console.WriteLine("2. Leap Year ");
+            Console.WriteLine("3. power of 2");
+            Console.WriteLine("4. Harmonic Number");
+            Console.WriteLine("5. Factors");
+            Console.WriteLine("6. Quotient and Remainder");
+            Console.WriteLine("7. swap number");
+            Console.WriteLine("Enter Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Flip Coin");
+                    Flip flip = new Flip();
+                    flip.Coin();
+                    flip.Coin1();
+                    ; break;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
