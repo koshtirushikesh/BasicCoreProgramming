7cf029d886f4386a720ff34d39520b46bd6f4e62
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-02-07 09:54:47 +0530

    Swap two Numbers

---
+++ BasicCorePrograming/Swap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograming
+{
+    internal class Swap
+    {
+        public void SwapTwoNumber()
+        {
+            int a = 30, b = 50;
+            Console.WriteLine("a:" + a);
+            Console.WriteLine("b:" + b);
+
+            int temp = b;
+            b = a;
+            a = temp;
+
+            Console.WriteLine("a:" + a);
+            Console.WriteLine("b:" + b);
+        }
+    }
+}
