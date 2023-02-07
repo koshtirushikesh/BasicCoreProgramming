2969b3a1af3f4a8d0e7bd0211602cd7603058229
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-02-07 10:17:01 +0530

    All File

---
+++ BasicCorePrograming/Factors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograming
+{
+    internal class Factors
+    {
+        public void FactorNumers() 
+        {
+            Console.WriteLine("Compute the prime factorization of N using brute force.");
+            Console.WriteLine("Enter the value");
+            int num =Convert.ToInt32(Console.ReadLine());
+            for(int i=num; i<=num; i--)
+            {
+                
+
+            }
+        
+        }
+    }
+}
